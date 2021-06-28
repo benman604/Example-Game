0bad1f34cfc428c0a136e77dd4ac195c5fb3501e
Benjamin Man <benjamin.man1001@gmail.com>
2021-06-28 12:36:13 -0700

    Day 3
    Implemented basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 100;
+    Vector3 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.x = Input.GetAxis("Horizontal");
+        move.z = Input.GetAxis("Vertical");
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
