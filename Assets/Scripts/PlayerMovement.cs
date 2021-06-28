938bc93f9ee89c9328dd07ee2d134a0ffc6b53eb
Benjamin Man <benjamin.man1001@gmail.com>
2021-06-28 13:43:18 -0700

    Day 5
    Added player jumping on space pressed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed = 100;
     Vector3 move;
+
+    public float jumpHeight = 4.5f;
+    float gravity = -9.81f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +21,22 @@ public class PlayerMovement : MonoBehaviour
     {
         move.x = Input.GetAxis("Horizontal");
         move.z = Input.GetAxis("Vertical");
-        controller.Move(move * speed * Time.deltaTime);
 
         Vector3 player = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 deltaPosition = player - Input.mousePosition;
         float angle = -Mathf.Atan2(deltaPosition.y, deltaPosition.x) * Mathf.Rad2Deg;
         transform.localRotation = Quaternion.Euler(Vector3.up * angle);
+
+        gravity -= 9.81f * Time.deltaTime;
+        move.y = gravity;
+        controller.Move(move * speed * Time.deltaTime);
+        if (controller.isGrounded)
+        {
+            gravity = 0;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                gravity = jumpHeight;
+            }
+        }
     }
 }
