154644fcaf1381ea1fefc76bea00816a71c7edd8
Benjamin Man <benjamin.man1001@gmail.com>
2021-06-28 14:18:24 -0700

    Day 7
    Implement camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camPos = player.transform.position - offset;
+        transform.position = Vector3.Lerp(transform.position, camPos, 10 * Time.deltaTime);
+    }
+}
