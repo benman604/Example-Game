86237d33f02e08b7ae1affcb1872fa7ec0409692
Benjamin Man <benjamin.man1001@gmail.com>
2021-06-28 13:14:20 -0700

    Day 4
    Implement player rotation / look at mouse

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,5 +19,10 @@ public class PlayerMovement : MonoBehaviour
         move.x = Input.GetAxis("Horizontal");
         move.z = Input.GetAxis("Vertical");
         controller.Move(move * speed * Time.deltaTime);
+
+        Vector3 player = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 deltaPosition = player - Input.mousePosition;
+        float angle = -Mathf.Atan2(deltaPosition.y, deltaPosition.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(Vector3.up * angle);
     }
 }
