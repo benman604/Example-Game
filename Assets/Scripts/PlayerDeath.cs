df1514bccfb39a92b6acfd7af2ace27e4cf9dc42
Benjamin Man <benjamin.man1001@gmail.com>
2021-06-28 14:30:24 -0700

    Day 8
    Implement respawning / Checkpoint system

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    GameObject currentCheckpoint;
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "Checkpoint")
+        {
+            currentCheckpoint = hit.gameObject;
+        }
+        if(hit.gameObject.tag == "OnHitDie")
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Vector3 respawnPos = currentCheckpoint.transform.GetChild(0).transform.position;
+        transform.position = respawnPos;
+    }
+}
