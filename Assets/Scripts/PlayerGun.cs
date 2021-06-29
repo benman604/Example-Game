2f0a5dedf72b68c5dfbb5698ad534b0b2c1b39ec
Benjamin Man <benjamin.man1001@gmail.com>
2021-06-28 18:06:27 -0700

    Day 9
    Implemented gun + shooting

---
+++ Assets/Scripts/PlayerGun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGun : MonoBehaviour
+{
+    public GameObject bullet;
+    public float shootForce = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject mybullet = Instantiate(bullet, transform.position, Quaternion.identity);
+            mybullet.gameObject.name = "PlayerBullet";
+            Vector3 direction = Quaternion.AngleAxis(-90, Vector3.up) * transform.forward;
+            mybullet.GetComponent<Rigidbody>().AddForce(direction * shootForce);
+            Destroy(mybullet, 2);
+        }
+    }
+}
