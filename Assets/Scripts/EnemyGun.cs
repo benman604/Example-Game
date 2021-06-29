be6e4c48d07ecbc05dbc5256d4a0d6b2661b686c
Benjamin Man <benjamin.man1001@gmail.com>
2021-06-28 18:55:32 -0700

    Day 10
    Implement enemy movement and shooting

---
+++ Assets/Scripts/EnemyGun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour
+{
+    public float viewDistance = 10;
+    public float shootInterval = 0.5f;
+    public float shootForce = 1000;
+    public GameObject player;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < viewDistance)
+        {
+            GameObject mybullet = Instantiate(bullet, transform.position, Quaternion.identity);
+            mybullet.gameObject.name = "EnemyBullet";
+            Vector3 direction = Quaternion.AngleAxis(-90, Vector3.up) * transform.forward;
+            mybullet.GetComponent<Rigidbody>().AddForce(direction * shootForce);
+            Destroy(mybullet, 2);
+        }
+        Invoke("Shoot", shootInterval);
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float viewDistance = 10;
+    public float moveSpeed = 1;
+    public GameObject player;
+    public CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.transform.position) < viewDistance)
+        {
+            Vector3 deltaPosition = transform.position - player.transform.position;
+            float angle = -Mathf.Atan2(deltaPosition.z, deltaPosition.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(angle * Vector3.up);
+
+            Vector3 direction = Quaternion.AngleAxis(-90, Vector3.up) * transform.forward;
+            controller.Move(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+}
